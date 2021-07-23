705485beca02cf579d2fef8b9257af765422db27
limhizy15 <limhizy15@ajou.ac.kr>
2021-07-23 22:50:04 +0900

    [2021.07.23] 환경 달라질 거 대비 백업

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-MEAN!!
+https://www.a-mean-blog.com/ko/blog/MEAN-Stack/_/MEAN-Stack-%EC%86%8C%EA%B0%9C
+
+자바스크립트로 따라해보기 🌸
--- index.js
+++ index.js
@@ -3,8 +3,20 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res){
-    res.send("Hello World!");
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res){
+//     res.send("Hello World!");
+// });
+
+app.get('/hello', function(req, res){
+    // 첫번째 매개변수 : ejs 이름 두번째 : 사용될 object
+    res.render('hello', {name: req.query.nameQuery})
+});
+
+app.get('/hello/:nameParam', function(req, res){
+    res.render('hello', {name: req.params.nameParam})
 });
 
 var port = 3000;
